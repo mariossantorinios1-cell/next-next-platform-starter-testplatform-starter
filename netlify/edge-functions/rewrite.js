d8c4c9f8d63d38022d17ae2e81a36b08fbe07315
mariossantorinios1-cell <marios.santorinios1@gmail.com>
2025-11-13 21:52:52 +0200

    Update rewrite.js

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,10 +1,4 @@
-const rewrite = async (request, context) => {
-    const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
-    return new URL(path, request.url);
-};
-
-export const config = {
-    path: '/edge'
-};
-
-export default rewrite;
+// temporarily disabled edge function for vercel
+export default function handler() {
+  return new Response("Edge function disabled for now");
+}
